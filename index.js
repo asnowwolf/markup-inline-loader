e83fef00824bf908b12ed52762e621c7ce5aeddd
Zhicheng Wang <zcwang@thoughtworks.com>
2016-07-12 09:49:15 +0800

    feature: apply svgo before import

--- index.js
+++ index.js
@@ -2,18 +2,36 @@ var PATTERN = /<(svg|img|math)\s+(.*?)src="(.*?)"(.*?)\/?>/gi;
 
 var fs = require('fs');
 var path = require('path');
+var SVGO = require('svgo');
+
+var svgo = new SVGO({
+  plugins: [
+    {
+      removeTitle: true
+    }
+  ]
+});
 
 module.exports = function (content) {
   var loader = this;
   content = content.replace(PATTERN, function (match, element, preAttributes, fileName, postAttributes) {
-    if (path.extname(fileName).toLowerCase() !== '.svg' && element.toLowerCase() === 'img') {
+    var isSvgFile = path.extname(fileName).toLowerCase() === '.svg';
+    var isImg = element.toLowerCase() === 'img';
+
+    if (!isSvgFile && isImg) {
       return match;
     }
+
     var filePath = path.join(loader.context, fileName);
-    var svg = fs.readFileSync(filePath, {encoding: 'utf-8'});
     loader.addDependency(filePath);
-    svg = svg.replace(/<\?xml.*?\?>\s*/, '');
-    return svg.replace(/^<svg/, '<svg ' + preAttributes + postAttributes + ' ');
+    var fileContent = fs.readFileSync(filePath, {encoding: 'utf-8'});
+    if (isSvgFile) {
+      // It's callback, But it's sync
+      svgo.optimize(fileContent, function (result) {
+        fileContent = result.data;
+      });
+    }
+    return fileContent.replace(/^<svg/, '<svg ' + preAttributes + postAttributes + ' ');
   });
   return content;
 };
